77beeb97bf4e81985d1922cea090bbc59640f009
r-lyeh <r-lyeh@users.noreply.github.com>
2016-04-08 10:54:15 +0200

    Update travis.build.sh

--- travis.build.sh
+++ travis.build.sh
@@ -16,7 +16,12 @@ if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi
 echo building...
 $CXX -v
 
-for f in *.c;   do [ ! -e "$f" ] && echo || $CC  $f -I. -c                                  -DNDEBUG -O2                       -pthread -fopenmp ; done
-for f in *.cpp; do [ ! -e "$f" ] && echo || $CXX $f -I. -c             -std=c++11           -DNDEBUG -O2                       -pthread -fopenmp ; done
-for f in *.cc;  do [ ! -e "$f" ] && echo || $CXX $f -I. -o ${f%.*}.bin -std=c++11 $(ls *.o) -DNDEBUG -O2 -lX11 -lrt -lGL -lGLU -pthread -fopenmp ; done 
-for f in *.cxx; do [ ! -e "$f" ] && echo || $CXX $f -I. -o ${f%.*}.bin -std=c++11 $(ls *.o) -DNDEBUG -O2 -lX11 -lrt -lGL -lGLU -pthread -fopenmp ; done 
+for f in src/*.c;   do [ ! -e "$f" ] && echo || $CC  $f -I. -c                                  -DNDEBUG -O2                       -pthread -fopenmp ; done
+for f in src/*.cpp; do [ ! -e "$f" ] && echo || $CXX $f -I. -c             -std=c++11           -DNDEBUG -O2                       -pthread -fopenmp ; done
+for f in src/*.cc;  do [ ! -e "$f" ] && echo || $CXX $f -I. -o ${f%.*}.bin -std=c++11 $(ls *.o) -DNDEBUG -O2 -lX11 -lrt -lGL -lGLU -pthread -fopenmp ; done 
+for f in src/*.cxx; do [ ! -e "$f" ] && echo || $CXX $f -I. -o ${f%.*}.bin -std=c++11 $(ls *.o) -DNDEBUG -O2 -lX11 -lrt -lGL -lGLU -pthread -fopenmp ; done 
+
+for f in *.c;       do [ ! -e "$f" ] && echo || $CC  $f -I. -c                                  -DNDEBUG -O2                       -pthread -fopenmp ; done
+for f in *.cpp;     do [ ! -e "$f" ] && echo || $CXX $f -I. -c             -std=c++11           -DNDEBUG -O2                       -pthread -fopenmp ; done
+for f in *.cc;      do [ ! -e "$f" ] && echo || $CXX $f -I. -o ${f%.*}.bin -std=c++11 $(ls *.o) -DNDEBUG -O2 -lX11 -lrt -lGL -lGLU -pthread -fopenmp ; done 
+for f in *.cxx;     do [ ! -e "$f" ] && echo || $CXX $f -I. -o ${f%.*}.bin -std=c++11 $(ls *.o) -DNDEBUG -O2 -lX11 -lrt -lGL -lGLU -pthread -fopenmp ; done 
